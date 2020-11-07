9f9d622b292c977346ff957a7096bb4137a3816b
iceburg63 <iceburg63@users.noreply.github.com>
2020-11-07 15:30:15 +0900

    Update to Kitura-Next

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -28,8 +28,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.1.0"),
-        .package(url: "https://github.com/IBM-Swift/CZlib.git", .upToNextMinor(from: "0.1.0"))
+        .package(url: "https://github.com/Kitura-Next/Kitura.git", from: "2.1.0"),
+        .package(url: "https://github.com/Kitura-Next/CZlib.git", .upToNextMinor(from: "0.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
